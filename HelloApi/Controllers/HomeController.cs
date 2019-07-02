45fd222b7c2984717fe0bbc9168bb5e2b025f772
Xin Zhao <xin@savysoda.com>
2019-07-02 23:57:02 +1000

    First

--- HelloApi/Controllers/HomeController.cs
+++ HelloApi/Controllers/HomeController.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
 namespace HelloApi.Controllers
 {
-    public class HomeController
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
     {
-        
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello API";
+        }
     }
 }
